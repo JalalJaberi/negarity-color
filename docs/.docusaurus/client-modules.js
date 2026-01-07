42974364c8d97b26186a050befe898bff9530b7d
prodo <proodoo@gmail.com>
2026-01-07 19:38:51 +0100

    Deploy website - based on 6669a4c9a8eadc053cf38fae06cd05c5515f2bd2

--- docs/.docusaurus/client-modules.js
+++
@@ -1,7 +0,0 @@
-export default [
-  require("/home/prodo/Documents/Projects/Negarity/negarity-color/docs/.docusaurus/docusaurus-plugin-css-cascade-layers/default/layers.css"),
-  require("/home/prodo/Documents/Projects/Negarity/negarity-color/docs/node_modules/infima/dist/css/default/default.css"),
-  require("/home/prodo/Documents/Projects/Negarity/negarity-color/docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/home/prodo/Documents/Projects/Negarity/negarity-color/docs/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/home/prodo/Documents/Projects/Negarity/negarity-color/docs/src/css/custom.css"),
-];
